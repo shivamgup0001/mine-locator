80edc5826175856f0923e8f97586522fb4373400
shivamgup0001 <shivamgup00001@gmail.com>
2021-07-30 15:31:33 +0530

    second commit

--- app/src/main/java/com/example/mine_locator/MainActivity.java
+++ app/src/main/java/com/example/mine_locator/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.mine_locator;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button button2;
+    private Button button1;
+    private Button button3;
+    public static int numbomb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new custom(this));
+        setContentView(R.layout.activity_main);
+        button1=findViewById(R.id.button1);
+        button2=findViewById(R.id.button2);
+        button3=findViewById(R.id.button3);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View v) {
+                set(20);
+                setContentView(new custom(MainActivity.this));
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View v) {
+                set(8);
+                setContentView(new custom(MainActivity.this));
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View v) {
+                set(30);
+                setContentView(new custom(MainActivity.this));
+            }
+        });
+    }
+    public static int get() {
+        return numbomb;
+    }
+    public void set(int num) {
+        this.numbomb = num;
     }
 }
--- app/src/main/java/com/example/mine_locator/custom.java
+++ app/src/main/java/com/example/mine_locator/custom.java
@@ -2,6 +2,7 @@ package com.example.mine_locator;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -13,33 +14,52 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import android.os.Vibrator;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static android.content.Context.VIBRATOR_SERVICE;
+
 
 public class custom extends View {
     private Rect rect;
     private Paint paint;
     private Rect rect1;
     private Paint paint1;
-    private Rect rect2;
     private Paint paint2;
+    private Paint paint3;
+    private Paint paint4;
+    private Paint paint5;
     private int i;
     private int j;
-    private int mleft[];
-    private int mright[];
-    private int mtop[];
-    private int mbottom[];
-    private  static int index=0;
-    private int k=0;
-
+    private int k;
+    private int ar[][];
+    private int l;
+    private int m;
+    private int bomb[];
+    private Vibrator v;
+    public int check = 0;
+    private int score = 0;
+    private Random rand;
+    private int count[][];
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public custom(Context context) {
         super(context);
+        v = (Vibrator) context.getSystemService(VIBRATOR_SERVICE);
         init(null);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public custom(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init(attrs);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public custom(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init(attrs);
@@ -52,125 +72,147 @@ public class custom extends View {
         init(attrs);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @SuppressLint("ResourceAsColor")
     private void init(@Nullable AttributeSet set) {
-rect=new Rect();
-paint= new Paint();
-        rect1=new Rect();
-        paint1= new Paint();
-        rect2=new Rect();
-        paint2= new Paint();
-paint.setColor(Color.BLACK);
-paint1.setColor(Color.WHITE);
+        rect = new Rect();
+        paint = new Paint();
+        rect1 = new Rect();
+        paint1 = new Paint();
+        paint2 = new Paint();
+        paint3 = new Paint();
+        paint4 = new Paint();
+        paint5 = new Paint();
+        rand = new Random();
+        paint.setColor(Color.BLACK);
+        paint1.setColor(Color.WHITE);
         paint2.setColor(Color.GREEN);
-        mright=new int[1000];
-        mleft=new int[1000];
-        mtop=new int[1000];
-        mbottom=new int[1000];
-        for(i=0;i<64;i++)
+        paint3.setColor(Color.RED);
+        paint4.setColor(Color.BLUE);
+        paint5.setColor(Color.BLUE);
+        paint3.setTextSize(90);
+        paint4.setTextSize(90);
+        paint5.setTextSize(60);
+        ar = new int[8][8];
+        count = new int[8][8];
+        bomb = new int[64];
+
+        for(i=0;i<MainActivity.get();i++)
         {
-            mright[i]=0;
-            mleft[i]=0;
-            mtop[i]=0;
-            mbottom[i]=0;
+            int u= ThreadLocalRandom.current().nextInt(0,63);
+            bomb[i]=u;
+        }
+        for (i = 0; i < 8; i++) {
+            for (j = 0; j < 8; j++) {
+                ar[i][j] = 0;
+            }
+        }
+        for (i = 0; i < 8; i++) {
+            for (j = 0; j < 8; j++) {
+                count[i][j] = 0;
+            }
         }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-canvas.drawColor(Color.YELLOW);
-            for (i = 1; i < 5; i++) {
-                for (j = 0; j < 4; j++) {
-                    rect.set(getWidth() / 2 - (80 * i), getHeight() / 2 + (80 * j), getWidth() / 2 - (80 * i) + 80, getHeight() / 2 + (80 * j) + 80);
-                    rect1.set(getWidth() / 2 - (80 * i) + 10, getHeight() / 2 + (80 * j) + 10, getWidth() / 2 - (80 * i) + 70, getHeight() / 2 + (80 * j) + 70);
-                    canvas.drawRect(rect, paint);
-                    canvas.drawRect(rect1, paint1);
-
-                }
+        canvas.drawColor(Color.YELLOW);
+        rect.set(getWidth() / 2 - 320, getHeight() / 2 - 320, getWidth() / 2 + 320, getHeight() / 2 + 320);
+        canvas.drawRect(rect, paint);
+        for (i = 0; i < 8; i++) {
+            for (j = 0; j < 8; j++) {
+                if (ar[i][j] == 1)
+                    score++;
             }
-            for (i = 1; i < 5; i++) {
-                for (j = 1; j < 5; j++) {
-                    rect.set(getWidth() / 2 - (80 * i), getHeight() / 2 - (80 * j), getWidth() / 2 - (80 * i) + 80, getHeight() / 2 - (80 * j) + 80);
-                    rect1.set(getWidth() / 2 - (80 * i) + 10, getHeight() / 2 - (80 * j) + 10, getWidth() / 2 - (80 * i) + 70, getHeight() / 2 - (80 * j) + 70);
-                    canvas.drawRect(rect, paint);
+        }
+        canvas.drawText("Score : " + score, getWidth() / 3 - 60, 120, paint3);
+
+        for (i = 0; i < 8; i++) {
+            for (j = 0; j < 8; j++) {
+                rect1.set(getWidth() / 2 - 310 + 80 * j, getHeight() / 2 - 310 + 80 * i, getWidth() / 2 - 310 + 60 + 80 * j, getHeight() / 2 - 310 + 60 + 80 * i);
+                if (ar[i][j] == 0)
                     canvas.drawRect(rect1, paint1);
-
+                if (ar[i][j] == 1) {
+                    canvas.drawRect(rect1, paint2);
+                    canvas.drawText("" + count[i][j], rect1.left + 15, rect1.bottom - 15, paint5);
                 }
-            }
-            for (i = 0; i < 4; i++) {
-                for (j = 1; j < 5; j++) {
-                    rect.set(getWidth() / 2 + (80 * i), getHeight() / 2 - (80 * j), getWidth() / 2 + (80 * i) + 80, getHeight() / 2 - (80 * j) + 80);
-                    rect1.set(getWidth() / 2 + (80 * i) + 10, getHeight() / 2 - (80 * j) + 10, getWidth() / 2 + (80 * i) + 70, getHeight() / 2 - (80 * j) + 70);
-                    canvas.drawRect(rect, paint);
-                    canvas.drawRect(rect1, paint1);
-
+                if (ar[i][j] == 2) {
+                    canvas.drawRect(rect1, paint3);
+                   check=1;
+                    canvas.drawText("Game Over !", getWidth() / 4 - 40, 200, paint4);
+                    SharedPreferences sh = getContext().getSharedPreferences("pong", Context.MODE_PRIVATE);
+                    int a = sh.getInt("highscore", 0);
+                    if (a < score) {
+                        SharedPreferences.Editor myEdit = sh.edit();
+                        myEdit.putInt("highscore", score);
+                        canvas.drawText("High Score:" + score, canvas.getWidth() / 4 - 40, 290, paint4);
+                        myEdit.apply();
+                    } else {
+                        canvas.drawText("High Score:" + a, canvas.getWidth() / 4 - 40, 290, paint4);
+                    }
+                    v.vibrate(400);
                 }
-            }
-            for (i = 0; i < 4; i++) {
-                for (j = 0; j < 4; j++) {
-                    rect.set(getWidth() / 2 + (80 * i), getHeight() / 2 + (80 * j), getWidth() / 2 + (80 * i) + 80, getHeight() / 2 + (80 * j) + 80);
-                    rect1.set(getWidth() / 2 + (80 * i) + 10, getHeight() / 2 + (80 * j) + 10, getWidth() / 2 + (80 * i) + 70, getHeight() / 2 + (80 * j) + 70);
-                    canvas.drawRect(rect, paint);
-                    canvas.drawRect(rect1, paint1);
-
-                }
-            }
-
-
-        if((mleft[index]!=0)&&(mright[index]!=0)&&(mtop[index]!=0)&&(mbottom[index]!=0)) {
-
-            if (k == 1) {
-
-                for(i=0;i<=index;i++) {
-                    rect2.set(mleft[i], mtop[i], mright[i], mbottom[i]);
-                    canvas.drawRect(rect2, paint2);
-                }
-                k = 0;
-                index++;
             }
         }
 
+        score = 0;
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event)
-    {
-        boolean value= super.onTouchEvent(event);
+    public boolean onTouchEvent(MotionEvent event) {
+        boolean value = super.onTouchEvent(event);
 
-        switch (event.getAction())
-        {
-            case MotionEvent.ACTION_DOWN:
-            {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
 
                 return true;
             }
             case MotionEvent.ACTION_MOVE: {
-                int left=getWidth()/2-310;
-                int top=getHeight()/2-310;
+                int left = getWidth() / 2 - 310;
+                int top = getHeight() / 2 - 310;
                 float x = event.getX();
                 float y = event.getY();
                 for (i = 0; i < 8; i++) {
-                    for(j=0;j<8;j++){
-                       if((x>(left+(80*i)))&&(x<(left+(80*i)+60))&&(y>(top+(80*j)))&&(y<(top+(80*j)+60))){
+                    for (j = 0; j < 8; j++) {
+                        if ((x > (left + (80 * j))) && (x < (left + (80 * j) + 60)) && (y > (top + (80 * i))) && (y < (top + (80 * i) + 60))) {
+                            if (ar[i][j] == 0) {
+                                ar[i][j] = 1;
+                                for (k = 0; k < MainActivity.get(); k++) {
 
+                                    l = bomb[k] / 8;
+                                    m = bomb[k] % 8;
+                                    //if (ar[i][j] == 1) {
+                                    if (l == i - 1) {
+                                        if (m == j || m == j - 1 || m == j + 1)
+                                            count[i][j]++;
+                                    }
+                                    if (l == i) {
+                                        if (m == j - 1 || m == j + 1)
+                                            count[i][j]++;
+                                    }
+                                    if (l == i + 1) {
+                                        if (m == j || m == j - 1 || m == j + 1)
+                                            count[i][j]++;
+                                    }
 
-                               mleft[index] = left + (80 * i);
-                               mright[index] = left + (80 * i) + 60;
-                               mtop[index] = top + (80 * j);
-                               mbottom[index] = top + (80 * j) + 60;
-                               k=1;
-                           postInvalidate();
+                                    if (i == l && j == m) {
+                                        ar[i][j] = 2;
 
-                       }
+                                    }
+                                }
+                            }
+                        }
+                        if (check == 0)
+                            postInvalidate();
+
                     }
                 }
-
-                return value;
             }
 
+            return value;
         }
-
         return value;
     }
 }
+
